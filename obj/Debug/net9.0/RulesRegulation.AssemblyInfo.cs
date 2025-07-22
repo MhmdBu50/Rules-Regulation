18522a7af2d90d95825af2fdbc98544f7209a50d
Ananas84 <jalilmuh123@gmail.com>
2025-07-22 18:06:04 +0300

    fixed shit

--- obj/Debug/net9.0/RulesRegulation.AssemblyInfo.cs
+++ obj/Debug/net9.0/RulesRegulation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RulesRegulation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e56150d8095e9e9afb2a87e6c0cf033c01e63e10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca5d5c663e755252802f31571e5a44c874aa02fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("RulesRegulation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RulesRegulation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
