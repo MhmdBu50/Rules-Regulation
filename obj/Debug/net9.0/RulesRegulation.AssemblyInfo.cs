107956c4f7887657c9ca65174f27be61a7755e5c
xAzzamx <azzam3mz@gmail.com>
2025-07-22 14:15:32 +0300

    connected the paths for svgs (after putting it in folders)

--- obj/Debug/net9.0/RulesRegulation.AssemblyInfo.cs
+++ obj/Debug/net9.0/RulesRegulation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RulesRegulation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2e902a411c88a54e7544e02998835023493dfc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a031c24a0f1514c4e2c4d6670eff38468c0ed7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("RulesRegulation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RulesRegulation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
